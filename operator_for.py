5aea646f5872bb9daf25fece9dfca0b6873a954d
Vasiliy <v_derkach@inbox.ru>
2024-05-15 21:10:58 +0500

    is for

---
+++ operator_for.py
@@ -0,0 +1,6 @@
+car_list = [ "BMW", "MB", "LADA", "KIA", "HONDA" ]
+cars_count = 0
+for car in car_list:
+    print( 'Я езжу на автомабиле марки ', car )
+    cars_count += 10
+print( 'cars_count = ', cars_count )
