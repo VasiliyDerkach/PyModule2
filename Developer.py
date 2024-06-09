86bc20a120c5d0d77f3ce906fe122055cf98e480
Vasiliy <v_derkach@inbox.ru>
2024-06-09 14:10:14 +0500

    class1

--- Developer.py
+++ Developer.py
@@ -4,12 +4,15 @@ class House:
         self.number_of_floors = number_of_floors
     def go_to(self,new_floor):
         if 1<= new_floor<= self.number_of_floors:
-            print('Этаж ', new_floor)
+            for i in range(1,new_floor+1):
+                print('Этаж ', i)
         else:
             print("Такого этажа не существует")
 
 home1= House('ЖК Эльбрус', 30)
-home1.go_to(2)
+home1.go_to(5)
 home1.go_to(35)
+h2 = House('Домик в деревне', 2)
+h2.go_to(5)
 
 
